105a4be35faedf1fa76e54458bfc012ceded076c
Nachiketa Pandey <81499541+nacpa@users.noreply.github.com>
2022-08-11 17:08:58 +0530

    Time: 0 ms (100.00%), Space: 6.9 MB (72.55%) - LeetHub

--- 54-spiral-matrix/54-spiral-matrix.cpp
+++ 54-spiral-matrix/54-spiral-matrix.cpp
@@ -7,23 +7,19 @@ public:
         int col=matrix[0].size();
         int left=0, right=col-1,top=0,bottom=row-1;
         int direction=0;
-        
         while(left<=right&&top<=bottom){
-            
                  if(direction==0){
                     for(int i= left; i<=right;i++) {
                     ans.push_back(matrix[top][i]);}
                     top++;
                     direction=1;
                 }
-            
             else if(direction ==1){
                 for(int i= top; i<=bottom;i++) {
                     ans.push_back(matrix[i][right]);}
                     direction=2;
                     right--; 
-            }
-                
+            }  
             else if(direction==2){
                 
                 for(int i= right; i>=left;i--) {
@@ -32,7 +28,6 @@ public:
                     bottom--; 
                 
             }
-            
             else if(direction==3){
                 
                 for(int i= bottom; i>=top;i--) {
@@ -42,9 +37,7 @@ public:
                 
             }
         
-    }
-        
-        
+        }
         return ans;
     }
 };

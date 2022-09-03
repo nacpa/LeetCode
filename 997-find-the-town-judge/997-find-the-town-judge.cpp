5743d5b2dd00edb89e6a229a86e832eb46e25322
Nachiketa Pandey <81499541+nacpa@users.noreply.github.com>
2022-09-03 20:22:55 +0530

    Time: 353 ms (25.78%), Space: 60.8 MB (81.65%) - LeetHub

---
+++ 997-find-the-town-judge/997-find-the-town-judge.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int findJudge(int n, vector<vector<int>>& trust) {
+        vector<int> nacpa(n+1,0);
+        for(auto& i: trust){
+            nacpa[i[0]]--;
+            nacpa[i[1]]++;
+        }
+        
+        for(int i=1;i<=n;i++){
+            if(nacpa[i]==n-1) return i;
+        }
+        return -1;
+        
+    }
+};

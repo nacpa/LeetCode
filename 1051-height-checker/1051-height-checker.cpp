1b5e626791c2f580355b5ba71990825b657320c8
Nachiketa Pandey <81499541+nacpa@users.noreply.github.com>
2022-09-04 20:54:25 +0530

    Time: 0 ms (100.00%), Space: 8.2 MB (24.20%) - LeetHub

--- 1051-height-checker/1051-height-checker.cpp
+++ 1051-height-checker/1051-height-checker.cpp
@@ -4,7 +4,6 @@ public:
         vector<int> temp(heights);
         sort(heights.begin(),heights.end());
         int ans=0;
-        
         for(int i=0;i<heights.size();i++){
             if(heights[i]!=temp[i])
                 ans++;

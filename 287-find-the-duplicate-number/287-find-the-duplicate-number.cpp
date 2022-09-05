483a80db0df17eaa0edab37c80b2f0a94df8c912
Nachiketa Pandey <81499541+nacpa@users.noreply.github.com>
2022-09-05 22:41:41 +0530

    Time: 382 ms (11.60%), Space: 83.8 MB (19.39%) - LeetHub

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
@@ -4,9 +4,7 @@ public:
         unordered_map<int,int> map;
         for(auto& i:nums){
             map[i]++;
-        }
-        for(auto& i: map){
-            if(i.second>1) return i.first;
+            if(map[i]>1)return i;
         }
         return -1;
     }

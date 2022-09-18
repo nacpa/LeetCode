46091c193d0748a23b73fc2ed152301b06f72d9c
Nachiketa Pandey <81499541+nacpa@users.noreply.github.com>
2022-09-18 23:37:59 +0530

    Time: 4 ms (36.92%), Space: 7.8 MB (49.64%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int func(int n,vector<int> &nums, vector<int> &dp){
+    int func(int n,vector<int> &nums, vector<int>&dp){
         if(n==0)return nums[n];
         if(n<0) return 0;
         if(dp[n]!= -1) return dp[n];

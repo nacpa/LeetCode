d33cffd1b834f9b234d5022e6d5c8d553adb95c1
Nachiketa Pandey <81499541+nacpa@users.noreply.github.com>
2022-09-21 22:06:42 +0530

    Time: 12 ms (42.39%), Space: 9 MB (32.46%) - LeetHub

---
+++ 120-triangle/120-triangle.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int func(int i ,int j, vector<vector<int>>& tri,int n,vector<vector<int>> &dp){
+    if(i==n-1) return tri[i][j];
+    if(dp[i][j]!=-1) return dp[i][j];
+    int dia = tri[i][j] +func(i+1,j+1,tri,n,dp);
+    int down =tri[i][j]+func(i+1,j,tri,n,dp);
+    return dp[i][j]= min(dia,down);
+    
+    
+}
+    
+    int minimumTotal(vector<vector<int>>& tri) {
+        int n=tri.size();
+        vector<vector<int>> dp(n,vector<int>(n,-1));
+        return func(0,0,tri,n,dp);
+        
+    }
+};

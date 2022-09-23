bd24e0532922590660b85593c7a74ecfd5eb6aaf
Nachiketa Pandey <81499541+nacpa@users.noreply.github.com>
2022-09-23 23:49:17 +0530

    Time: 36 ms (17.48%), Space: 10.3 MB (26.62%) - LeetHub

---
+++ 931-minimum-falling-path-sum/931-minimum-falling-path-sum.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+  int func(int i ,int j,vector<vector<int>> &mat,vector<vector<int>> &dp ){
+    if(j<0 || j >= mat.size()) return INT_MAX;
+    if(i== mat.size()-1) return mat[i][j];
+    if(dp[i][j]!=-1) return dp[i][j];
+    int down=  func(i+1,j,mat,dp);
+    int leftD=  func(i+1,j-1,mat,dp);
+    int rightD=  func(i+1,j+1,mat,dp);
+        dp[i][j]=mat[i][j]+ min(leftD,min(down,rightD));
+    return dp[i][j];
+    
+}
+    int minFallingPathSum(vector<vector<int>>& mat) {
+    int n =mat.size();
+    int ans= INT_MAX;
+    vector<vector<int>> dp(n,vector<int>(n,-1));
+    for(int i=0;i<n;i++){
+        ans=min(ans , func(0,i,mat,dp)) ;
+    }
+    
+    return ans;
+        
+    }
+};
